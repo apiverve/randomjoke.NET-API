3c5fc22f9d37427e355323a66f4b7c528b9e78ff
APIVerve Team <hello@apiverve.com>
2024-08-20 11:04:28 -0500

    Bug Fixes

--- RandomJokeAPIClient.cs
+++ RandomJokeAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, randomjokeQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, RandomJokeQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(randomjokeQueryOptions options = null)
+        public ResponseObj Execute(RandomJokeQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(randomjokeQueryOptions options)
+        private string constructURL(RandomJokeQueryOptions options)
         {
             string url = _apiEndpoint;
 
